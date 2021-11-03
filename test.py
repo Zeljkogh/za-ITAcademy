1abd52ea7bb74c33b3a4a405a967466feeda649b
Zeljkogh <93686921+Zeljkogh@users.noreply.github.com>
2021-11-04 00:14:25 +0100

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-ime = input("Unesite vase ime:")#ovo je varijabla za unosenje imena
-godine = int(input("Unesite vase godine:"))
-print("Zdravo, ja se zovem "+ime+ " Imam "+str(godine)+ " godina")#ovo je komentar
-"""
-ovo je komentar
-sada 
-"""
