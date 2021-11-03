894d60ec6bc34f80ba514ab46594cdd9fe725ac9
Zeljkogh <93686921+Zeljkogh@users.noreply.github.com>
2021-11-04 00:12:21 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+ime = input("Unesite vase ime:")#ovo je varijabla za unosenje imena
+godine = int(input("Unesite vase godine:"))
+print("Zdravo, ja se zovem "+ime+ " Imam "+str(godine)+ " godina")#ovo je komentar
+"""
+ovo je komentar
+sada 
+"""
